c88dfc472f3adcd650a6a566a94c834a9652cbf6
王以珩 <YihengWang828@users.noreply.github.com>
2020-05-21 14:53:12 +0800

    修改时间选择器
    注：活动描述垂直居中会导致填写时上部分空缺。

--- pages/newAct/newAct.js
+++ pages/newAct/newAct.js
@@ -1,43 +1,6 @@
 // pages/newAct/newAct.js
-// 活动时间时间选择器
-const date = new Date();
-const years = [];
-const months = [];
-const days = [];
-const hours = [];
-const minutes = [];
-//获取年
-for (let i = 2018; i <= date.getFullYear() + 5; i++) {
-  years.push("" + i);
-}
-//获取月份
-for (let i = 1; i <= 12; i++) {
-  if (i < 10) {
-    i = "0" + i;
-  }
-  months.push("" + i);
-}
-//获取日期
-for (let i = 1; i <= 31; i++) {
-  if (i < 10) {
-    i = "0" + i;
-  }
-  days.push("" + i);
-}
-//获取小时
-for (let i = 0; i < 24; i++) {
-  if (i < 10) {
-    i = "0" + i;
-  }
-  hours.push("" + i);
-}
-//获取分钟
-for (let i = 0; i < 60; i++) {
-  if (i < 10) {
-    i = "0" + i;
-  }
-  minutes.push("" + i);
-}
+var util = require('../../utils/util.js');
+var dateTimePicker = require('../../utils/dateTimePicker.js');
 
 Page({
 
@@ -46,24 +9,20 @@ Page({
    */
   data: {
     time: '',
-    multiArray: [years, months, days, hours, minutes],
-    multiIndex: [0, 9, 16, 10, 17],
-    choose_year: '',
+    dateTimeArray: '',
+    dateTime: '',
+    startYear: 2020,
+    endYear: 2050,
     picker: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     region: ['四川省', '成都市', '双流区'],
-    date: '2020-01-01',
+    date: '',
+    minDate: '',
     imgList: [],
     picker2: ['普通活动', '特殊活动'],
   },
-  // 活动时间选择器
-  PickerChange(e) {
-    console.log(e);
-    this.setData({
-      index: e.detail.value
-    })
-  },
+
   //活动类型选择
-  PickerChangeAct(e){
+  PickerChangeAct(e) {
     console.log(e);
     this.setData({
       index2: e.detail.value
@@ -75,8 +34,14 @@ Page({
       date: e.detail.value
     })
   },
+
+  DateTimeChange(e) {
+    this.setData({
+      dateTime: e.detail.value
+    })
+  },
   // 地址选择器
-  RegionChange: function(e) {
+  RegionChange: function (e) {
     this.setData({
       region: e.detail.value
     })
@@ -132,143 +97,73 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
+  onLoad: function (options) {
+    var DATE = util.formatDate(new Date());
+    var TIME = dateTimePicker.dateTimePicker(this.data.startYear, this.data.endYear, util.formatDate2(new Date()))
+    var lastArray = TIME.dateTimeArray;
+    var lastTime = TIME.dateTime;
     this.setData({
-      choose_year: this.data.multiArray[0][0]
+      date: DATE,
+      minDate: DATE,
+      dateTime: TIME.dateTime,
+      dateTimeArray: TIME.dateTimeArray
     })
   },
-  //获取时间日期
-  bindMultiPickerChange: function(e) {
-    // console.log('picker发送选择改变，携带值为', e.detail.value)
+  changeDateTimeColumn(e) {
+    var arr = this.data.dateTime, dateArr = this.data.dateTimeArray;
+    arr[e.detail.column] = e.detail.value;
+    dateArr[2] = dateTimePicker.getMonthDay(dateArr[0][arr[0]], dateArr[1][arr[1]]);
     this.setData({
-      multiIndex: e.detail.value
-    })
-    const index = this.data.multiIndex;
-    const year = this.data.multiArray[0][index[0]];
-    const month = this.data.multiArray[1][index[1]];
-    const day = this.data.multiArray[2][index[2]];
-    const hour = this.data.multiArray[3][index[3]];
-    const minute = this.data.multiArray[4][index[4]];
-    // console.log(`${year}-${month}-${day}-${hour}-${minute}`);
-    this.setData({
-      time: year + '-' + month + '-' + day + ' ' + hour + ':' + minute
-    })
-    // console.log(this.data.time);
-  },
-  //监听picker的滚动事件
-  bindMultiPickerColumnChange: function(e) {
-    //获取年份
-    if (e.detail.column == 0) {
-      let choose_year = this.data.multiArray[e.detail.column][e.detail.value];
-      console.log(choose_year);
-      this.setData({
-        choose_year
-      })
-    }
-    //console.log('修改的列为', e.detail.column, '，值为', e.detail.value);
-    if (e.detail.column == 1) {
-      let num = parseInt(this.data.multiArray[e.detail.column][e.detail.value]);
-      let temp = [];
-      if (num == 1 || num == 3 || num == 5 || num == 7 || num == 8 || num == 10 || num == 12) { //判断31天的月份
-        for (let i = 1; i <= 31; i++) {
-          if (i < 10) {
-            i = "0" + i;
-          }
-          temp.push("" + i);
-        }
-        this.setData({
-          ['multiArray[2]']: temp
-        });
-      } else if (num == 4 || num == 6 || num == 9 || num == 11) { //判断30天的月份
-        for (let i = 1; i <= 30; i++) {
-          if (i < 10) {
-            i = "0" + i;
-          }
-          temp.push("" + i);
-        }
-        this.setData({
-          ['multiArray[2]']: temp
-        });
-      } else if (num == 2) { //判断2月份天数
-        let year = parseInt(this.data.choose_year);
-        console.log(year);
-        if (((year % 400 == 0) || (year % 100 != 0)) && (year % 4 == 0)) {
-          for (let i = 1; i <= 29; i++) {
-            if (i < 10) {
-              i = "0" + i;
-            }
-            temp.push("" + i);
-          }
-          this.setData({
-            ['multiArray[2]']: temp
-          });
-        } else {
-          for (let i = 1; i <= 28; i++) {
-            if (i < 10) {
-              i = "0" + i;
-            }
-            temp.push("" + i);
-          }
-          this.setData({
-            ['multiArray[2]']: temp
-          });
-        }
-      }
-      console.log(this.data.multiArray[2]);
-    }
-    var data = {
-      multiArray: this.data.multiArray,
-      multiIndex: this.data.multiIndex
-    };
-    data.multiIndex[e.detail.column] = e.detail.value;
-    this.setData(data);
+      dateTimeArray: dateArr,
+      dateTime: arr
+    });
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
+  onReady: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
+  onHide: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
+  onUnload: function () {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
 
   }
 })
